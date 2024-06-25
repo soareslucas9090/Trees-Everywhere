727d7c742c2371775d60f32443674484185dd29b
Lucas Soares <soareslukas9090@hotmail.com>
2024-06-25 13:17:00 -0300

    feat: criação do model User + primeiro migrate

--- trees/trees/everywhere/models.py
+++ trees/trees/everywhere/models.py
@@ -1,1 +1,106 @@
-# Create your models here.
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    Permission,
+    PermissionsMixin,
+)
+from django.db import models
+
+
+class Account(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    created = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True, null=False)
+
+
+class UserManager(BaseUserManager):
+    def create_user(
+        self,
+        email,
+        name,
+        account,
+        password=None,
+        **extra_fields,
+    ):
+        if not name:
+            raise ValueError("The user needs a valid name!")
+        if not email:
+            raise ValueError("The user needs a valid email!")
+        if not account:
+            raise ValueError("The user needs a valid account!")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+            account=Account.objects.get(pk=account),
+            **extra_fields,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(
+        self,
+        email,
+        nome,
+        account,
+        password=None,
+        **extra_fields,
+    ):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_admin", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_admin") is not True:
+            raise ValueError("Superuser must have is_admin=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        user = self.create_user(
+            email=email,
+            nome=nome,
+            account=account,
+            password=password,
+            **extra_fields,
+        )
+
+        permissions = Permission.objects.all()
+        user.user_permissions.set(permissions)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length=255, null=False)
+    email = models.EmailField(unique=True, null=False)
+    account = models.ForeignKey(
+        Account, related_name="user_account", on_delete=models.RESTRICT, null=False
+    )
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    REQUIRED_FIELDS = [
+        "name",
+    ]
+
+    USERNAME_FIELD = "email"
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.nome
+
+    def save(self, *args, **kwargs):
+        self.nome = self.nome.lower()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = "user"
+        verbose_name_plural = "users"
--- trees/trees/trees/settings.py
+++ trees/trees/trees/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -8,15 +9,55 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-mfty3)v!c*ozqpdjzetui*@8=l%fzi1p=ap31!1oe5(h)=@prj"
+SECRET_KEY = os.environ.get("secretKeyDjango")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if SECRET_KEY:
+    DEBUG = os.environ.get("debugMode")
+    ALLOWED_HOSTS = [os.environ.get("allowedHosts")]
+    DATABASES = {
+        "default": {
+            "ENGINE": os.environ.get("bdEngine"),
+            "NAME": os.environ.get("bdName"),
+            "USER": os.environ.get("bdUser"),
+            "PASSWORD": os.environ.get("bdPass"),
+            "HOST": os.environ.get("bdHost"),
+            "PORT": os.environ.get("bdPort"),
+        }
+    }
+else:
+    from .env import (
+        allowedHosts,
+        bdEngine,
+        bdHost,
+        bdName,
+        bdPass,
+        bdPort,
+        bdUser,
+        debug,
+        secretKeyDjango,
+    )
 
-ALLOWED_HOSTS = []
+    SECRET_KEY = secretKeyDjango
+    DEBUG = debug
+    ALLOWED_HOSTS = allowedHosts
+    DATABASES = {
+        "default": {
+            "ENGINE": bdEngine,
+            "NAME": bdName,
+            "USER": bdUser,
+            "PASSWORD": bdPass,
+            "HOST": bdHost,
+            "PORT": bdPort,
+        }
+    }
 
 
-# Application definition
+AUTH_USER_MODEL = "everywhere.User"
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = False
+
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -59,20 +100,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "trees.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -92,9 +119,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+LANGUAGE_CODE = "pt-br"
+
+TIME_ZONE = "America/Sao_Paulo"
 
 USE_I18N = True
 
