aa0884b1f9dc631dad7ccb5deb95a4f85f066fa0
Lucas Soares <soareslukas9090@hotmail.com>
2024-07-01 15:48:27 -0300

    fix: tentativa de rodar arquivos estáticos no server real time

--- trees/trees/settings.py
+++ trees/trees/settings.py
@@ -198,7 +198,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
