febe8573a5bb6e8cf976d18ceca11387f3ddb1ed
Lucas Soares <soareslukas9090@hotmail.com>
2024-07-01 16:30:48 -0300

    fix: tentativa de leitura de arquivos estaticos

--- trees/trees/settings.py
+++ trees/trees/settings.py
@@ -197,8 +197,14 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",
+]
+STATIC_ROOT = BASE_DIR / "static"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
